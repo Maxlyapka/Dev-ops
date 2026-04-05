1ced7fbce66ea16394553ba64ee574087c7d9a0e
Max <maks71800@gmail.com>
2026-04-05 15:36:21 +0300

    Create main.c

---
+++ scripts/main.c
@@ -0,0 +1,9 @@
+/* * Source code used to compile and build a custom .deb package 
+ * using 'checkinstall' tool for the DevOps project.
+ */
+#include <stdio.h>
+
+int main() {
+    printf("\nSUCCESS: DevOps Project Tool is installed and running!\n\n");
+    return 0;
+}
